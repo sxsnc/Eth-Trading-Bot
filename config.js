aa95c24aac70b4d35c639ebc53297ea09c153d3e
sxsnc <167494992+sxsnc@users.noreply.github.com>
2024-04-21 10:09:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var myaddress = "your public address"
-var myseed = "your seed to the address"
+var myaddress = "your public address" //example 0x4B8A4B6f0F90785b07bf72C14CD5F07692442056
+var myseed = "your seed to the address" //example pulp analyst crime energy lunar excite neck talent tattoo stumble fetch door
 var networks = "1" //1 = ETH , 2 = BNB , 3 = POLYGON
 var maxspend = "0.5" // max eth you want to spend. Note: Make sure you have that amount in the wallet you provided.
